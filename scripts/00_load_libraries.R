6286475d8b1d6f11ea984c2e96e4da36df6c530f
mchapak <mchapak@gmail.com>
2021-07-22 19:10:04 +0300

    updated

--- scripts/00_load_libraries.R
+++ scripts/00_load_libraries.R
@@ -1,3 +1,10 @@
-library(redcapAPI)
-library(REDCapR)
-library(RCurl)
+## Install and load packages
+
+rm(list=ls())
+
+# install.packages("pacman") # install this package first if not installed
+
+## Installing required packges
+pacman::p_load(RCurl,REDCapR,redcapAPI,tidyr,RColorBrewer,dplyr,leaflet,
+               ggthemes,lubridate,plotly,knitr,kableExtra, ggplot2,forcats,
+               flexdashboard)
