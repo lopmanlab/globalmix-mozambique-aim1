01f7be5d8b60c9e1659e79f1ed8c103db09e0207
mchapak <mchapak@gmail.com>
2021-09-27 21:11:35 +0300

    data dwnload script

--- scripts/00_data_download.R
+++ scripts/00_data_download.R
@@ -7,5 +7,5 @@ url <- "https://redcap.emory.edu/api/"
 data <- redcap_read(batch_size = 500L, interbatch_delay = 0.5, continue_on_error = TRUE, redcap_uri = url, token = token, records = NULL, 
                    records_collapsed = "", fields_collapsed = "")$data
 
-# save metadata file
+# save file
 rio::export(data, "raw/all_data_09242021.csv") ##### Save the file in your preferred directory
