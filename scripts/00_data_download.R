75e6a3c41791a6960d9b59e2c3389a0fa3214c4e
holin-chen <holinchen1996@gmail.com>
2022-03-02 15:46:25 -0800

    upload contact cleaning code and data

--- scripts/00_data_download.R
+++ scripts/00_data_download.R
@@ -1,11 +1,13 @@
 #!/usr/bin/env Rscript
 
 library(REDCapR)
-token <- "" # insert personal redcap token
+token <- "B90C5E7F7911BC2189D8367CAA4D771B" # insert personal redcap token
 url <- "https://redcap.emory.edu/api/"
 
 data <- redcap_read(batch_size = 500L, interbatch_delay = 0.5, continue_on_error = TRUE, redcap_uri = url, token = token, records = NULL, 
                    records_collapsed = "", fields_collapsed = "")$data
 
 # save file
-rio::export(data, "raw/all_data_09242021.csv") ##### Save the file in your preferred directory
+rio::export(data, "raw/all_data_03022022.csv") ##### Save the file in your preferred directory
+
+
