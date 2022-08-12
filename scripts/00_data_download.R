67c66a05cb712447408e7371537983b4e964d95e
Moses Chapa Kiti <mchapak@gmail.com>
2022-08-12 11:41:07 -0400

    updated scripts

--- scripts/00_data_download.R
+++ scripts/00_data_download.R
@@ -1,17 +1,15 @@
 #!/usr/bin/env Rscript
 
 library(REDCapR)
-token <- "B90C5E7F7911BC2189D8367CAA4D771B" # insert personal redcap token
+# token <- "B90C5E7F7911BC2189D8367CAA4D771B" # insert personal redcap token
+
+token <- "5B0CEE7AE22F71A04B7C863E0F458249"
 url <- "https://redcap.emory.edu/api/"
 
 data <- redcap_read(batch_size = 500L, interbatch_delay = 0.5, continue_on_error = TRUE, redcap_uri = url, token = token, records = NULL, 
                    records_collapsed = "", fields_collapsed = "")$data
 
 # save file
-<<<<<<< HEAD
-rio::export(data, "raw/all_data_09242021.csv") ##### Save the file in your preferred directory
-=======
-rio::export(data, "raw/all_data_03022022.csv") ##### Save the file in your preferred directory
+rio::export(data, "data/raw/all_data_08092022.RDS") ##### Save the file in your preferred directory
 
-
->>>>>>> cc7c3454b49acdff9c6b5e5f76f571e4a8f220c9
+# previous data downloaded on 07/05/2022
