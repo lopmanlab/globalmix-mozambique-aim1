d10c7471d6c5f743f2a4e06ecdcdabb07a84ee28
Moses Chapa Kiti <mchapak@gmail.com>
2022-12-13 19:27:49 -0500

    updated code

--- scripts/00_load_libraries.R
+++ scripts/00_load_libraries.R
@@ -5,7 +5,7 @@ rm(list=ls())
 # install.packages("pacman") # install this package first if not installed
 
 ## Installing required packges
-pacman::p_load(tidyr, RColorBrewer, dplyr, leaflet,
-               ggthemes, lubridate, plotly, knitr, kableExtra, ggplot2, forcats,
-               flexdashboard, igraph, table1, kableExtra, gtsummary)
+pacman::p_load(tidyr, dplyr, ggplot2, plotly,
+               ggthemes, lubridate, knitr, kableExtra, 
+               table1, kableExtra, gtsummary)
 # RCurl, REDCapR, redcapAPI, 
