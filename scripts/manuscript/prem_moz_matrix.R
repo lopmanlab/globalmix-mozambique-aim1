5b5687cd4043e6813c7284660212eea64f882713
Kim <SKIM689@emory.edu>
2023-09-13 17:52:45 -0400

    updated contact matrices and transmission model to fit new age groups.

--- scripts/manuscript/prem_moz_matrix.R
+++ scripts/manuscript/prem_moz_matrix.R
@@ -20,14 +20,14 @@ moz_prem <- read.csv("C:/Users/skim689/OneDrive - Emory University/Desktop/Mixin
 ### Generate data frame of participant age groups for orig prem matrix matched to age groups for target matrix
 age_cat_part <- data.frame(part_age1=c("0_4","5_9","10_14","15_19","20_24","25_29","30_34","35_39",
                                        "40_44","45_49","50_54","55_59","60_64","65_69","70_74","75"),
-                           part_age2 =c("0_9", "0_9", "10_19","10_19", "20_29", "20_29", "30_39", "30_39", 
-                                        "40_49","40_49", "50_59", "50_59",  "60", "60", "60", "60"))
+                           part_age2 =c("[0,9)", "[0,9)", "[10,19)","[10,19)", "[20,29)", "[20,29)", "[30,39)", "[30,39)", 
+                                        "[40,59)","[40,59)", "[40,59)", "[40,59)",  "60+", "60+", "60+", "60+"))
 
 
 ### Generate data frame of contact age groups for orig prem matrix matched to contact age groups for target matrix
 age_cat_cont <- data.frame(cont_age1 = colnames(moz_prem),
-                           cont_age2 = c("0_9", "0_9", "10_19","10_19", "20_29", "20_29", "30_39", "30_39", 
-                                         "40_49","40_49", "50_59", "50_59",  "60", "60", "60", "60"))
+                           cont_age2 = c("[0,9)", "[0,9)", "[10,19)","[10,19)", "[20,29)", "[20,29)", "[30,39)", "[30,39)", 
+                                         "[40,59)","[40,59)", "[40,59)", "[40,59)",  "60+", "60+", "60+", "60+"))
 
 moz_prem$part_age1 <- age_cat_part$part_age1
 
@@ -46,7 +46,7 @@ moz_prem <-moz_prem %>%
 moz_pop_5yr <- read.csv("C:/Users/skim689/OneDrive - Emory University/Desktop/Mixing Studies/Global Mix/Mozambique/Contact Patterns Abstract/Pop Sizes/agecat_5_total.csv")
 
 ### population data for 10-year population distribution in Moz
-moz_pop_10yr <- read.csv("C:/Users/skim689/OneDrive - Emory University/Desktop/Mixing Studies/Global Mix/Mozambique/Contact Patterns Abstract/Pop Sizes/agecat_10_total.csv")
+moz_pop_10yr <- read.csv("C:/Users/skim689/OneDrive - Emory University/Desktop/Mixing Studies/Global Mix/Mozambique/Contact Patterns Abstract/Pop Sizes/agecat_10_total_new.csv")
 
 
 ### "linearly" collapse the participant-contact cells based on population sizes
@@ -56,12 +56,12 @@ moz_prem10<- moz_prem %>%
   group_by(part_age2, cont_age2)%>%
   summarise(tot_pop_contacts = sum(tot_pop_contacts))%>% ##total contacts made between 0_9&0_9, 0_9&10_19 etc
   left_join(moz_pop_10yr, by = c("part_age2"="part_age2"))%>% # pop size of 10-year participant age band
-  mutate(avg_cont_new = tot_pop_contacts/pop10yr)        ## average contacts made using 10-year age band
+  mutate(contacts = tot_pop_contacts/pop10yr)        ## average contacts made using 10-year age band
 
 ### plot
-ggplot(moz_prem10, aes(x = part_age2, y = cont_age2, fill = avg_cont_new)) + theme(legend.position = "bottom") + 
-  scale_fill_gradient2(low = "white", high = "#273871", mid = "#7FABD3", midpoint = 3.4, limit = c(0,7))+
+premmatrix <- ggplot(moz_prem10, aes(x = part_age2, y = cont_age2, fill = contacts)) + theme(legend.position = "bottom") + 
+  scale_fill_gradient2(low = "white", high = "#273871", mid = "#7FABD3", midpoint = 3.4, limit = c(0,6))+
   xlab("Age of participant")+ylab("Age of contact")+
   geom_tile()+
-  geom_text(aes(label=round(avg_cont_new, digits=2)), colour = "black", check_overlap = TRUE)+
+  geom_text(aes(label=round(contacts, digits=2)), colour = "black", check_overlap = TRUE)+
   theme()
